a02c13910b7483925e716406d8f9fb43c8f2e93d
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2020-10-30 23:19:57 +0800

    created scripts to automate templates

---
+++ in_progress/create_abc.sh
@@ -0,0 +1,7 @@
+mkdir ABC$1
+cd ABC$1
+
+cp ../template.cpp ABC$1A.cpp
+cp ../template.cpp ABC$1B.cpp
+cp ../template.cpp ABC$1C.cpp
+cp ../template.cpp ABC$1D.cpp
---
+++ in_progress/move_abc.sh
@@ -0,0 +1,2 @@
+cp -a ABC$1/. ../ABCs/
+rm -r ABC$1
