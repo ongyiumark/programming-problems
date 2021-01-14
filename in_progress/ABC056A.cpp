67a954afcfe711faf45ea5487bff8bfe4a8843ff
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2021-01-15 02:49:22 +0800

    Finished ABC056

---
+++ in_progress/ABC056A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+
+template <typename T>
+using ordered_set = __gnu_pbds::tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  char a, b; cin >> a >> b;
+  cout << ((a=='D')^(b=='D') ? "D" : "H") << endl;
+  return 0;
+}
