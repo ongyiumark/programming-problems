b88953cd382f8d84e0067680da6e7bd73ca6cab9
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2021-01-30 00:49:57 +0800

    Edited ABC097B

--- ABCs/ABC097B.cpp
+++ ABCs/ABC097B.cpp
@@ -1,9 +1,6 @@
 /*
-  Start from the top and check if its a perfect power.
-  
-  To check if its a perfect power, we prime factorize.
-  Let m be the minimum exponent in the prime factorization.
-  m must be at least 2, and all the other exponents must be a multiple of m.
+  Create a boolean array of perfect powers.
+  Do all perfect powers of 2, 3, 4, ... x-1.
 */
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -20,37 +17,25 @@ typedef pair<int,pair<int,int>> piii;
 template <typename T>
 using ordered_set = __gnu_pbds::tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-bool valid(int x){
-  if (x == 1) return true;
-  map<int,int> factors;
-  for (int i = 2; i*i <= x; i++){
-    while(x % i == 0){
-      factors[i]++;
-      x /= i;
-    }
-  }
-  if (x > 1) factors[x]++;
-
-  int p = 1e9;
-  for (auto it : factors){
-    p = min(p, it.second);
-  }
-  if (p == 1) return false;
-
-  bool valid = 1;
-  for (auto it : factors){
-    valid &= (it.second%p == 0);
-  }
-  return valid;
-}
-
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   
   int x; cin >> x;
+  bool perfect[x+1];
+  memset(perfect, 0, sizeof perfect);
+  perfect[1] = 1;
+
+  for (int b = 2; b < x; b++){
+    int e = b*b;
+    while(e <= x){
+      perfect[e] = 1;
+      e *= b;
+    }
+  }
+
   for (int i = x; i >= 1; i--){
-    if (valid(i)) {
+    if (perfect[i]){
       cout << i << endl;
       return 0;
     }
