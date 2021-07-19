063f4103728f729c2408b9a5c6776f423d654d88
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2021-07-20 01:13:02 +0800

    Worked on CF102920

--- in_progress/Uncompleted Contests/CF102920/h.cpp
+++ in_progress/Uncompleted Contests/CF102920/h.cpp
@@ -13,33 +13,11 @@ typedef pair<int,pair<int,int>> piii;
 template <typename T>
 using ordered_set = __gnu_pbds::tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-const int N = 6e4+1;
-bitset<2*N+5> holes[3];
-
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int n[3];
-  for (int i = 0; i < 3; i++){
-    cin >> n[i];
-    holes[i].reset();
-    for (int j = 0; j < n[i]; j++){
-      int x; cin >> x;
-      x += 30000;
-      if (i <= 1) holes[i].set(x);
-      else holes[i].set(2*N-x);
-    }
-  }
 
-  int cnt = 0;
-  for (int i = N; i >= 0; i--){
-    if (holes[1][i] == 0) {
-      holes[2] >>= 2;
-      continue;
-    }
-    cnt += (holes[0]&holes[2]).count();
-    holes[2] >>= 2;
-  }
-  cout << cnt << endl;
+  
+  
   return 0;
 }
